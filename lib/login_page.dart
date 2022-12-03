2ff2645bf6dfa0e145c3001ce81a08dec0b27f77
Ranuka Harshana <72878671+RanukaHarshana@users.noreply.github.com>
2022-12-03 10:32:37 +0530

    Body Container Update

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,8 +9,46 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+        leading: const Icon(
+          Icons.account_balance,
+          color: Colors.white,
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.4),
+              blurRadius: 4.0,
+              spreadRadius: 2.0,
+            )
+          ]
+        ),
+
+        width: 200,
+        height: 100,
+        child: const Center(
+          child:  Text('Login Page', style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+          ),
+        )
+      ),
+    );
   }
 }
